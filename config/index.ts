2db72b9b63a9455bc83a8406563a03cf2c5c97fa
Mike Hacker <desertsofcacti@gmail.com>
2019-08-15 00:00:00 -0500

    housekeeping

--- config/index.ts
+++
@@ -1,9 +0,0 @@
-import { Configuration } from './types';
-
-export const config = (environment?: string): Configuration => {
-  const env = environment || process.env.NODE_ENV;
-  if (env === 'production') {
-    return prod;
-  }
-  return dev;
-};
